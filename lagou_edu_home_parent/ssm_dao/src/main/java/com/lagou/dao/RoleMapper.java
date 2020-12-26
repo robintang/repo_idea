6bb258026894a365fec92ea3407257df6c711c4d
robin <robintang9325@gmail.com>
2020-12-26 21:46:29 +1100

    first commit

---
+++ lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/RoleMapper.java
@@ -0,0 +1,26 @@
+package com.lagou.dao;
+
+import com.lagou.domain.Role;
+import com.lagou.domain.Role_menu_relation;
+
+import java.util.List;
+
+public interface RoleMapper {
+
+    /*查询所有角色 条件查询*/
+    public List<Role> findAllRole(Role role);
+
+    /*根据id查询角色关联的菜单信息id*/
+    public List<Integer> findMenuByRoleId(Integer roleId);
+
+
+    /*根据roleid清空中间表关联关系*/
+    public void deleteRoleContextMenu(Integer rid);
+
+    /*为角色分配菜单信息*/
+    public void roleContextMenu(Role_menu_relation role_menu_relation);
+
+    /*删除角色*/
+   public void deleteRole(Integer id);
+
+}
